05b7c24bc08621ff9a7e8d3d8b73f34b5ce5575d
Hardik Sarkar <107950304+hardiksarkar@users.noreply.github.com>
2023-08-25 10:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let selectedElem = document.getElementById("colorSelect");
+let allOptions = Array.from(selectedElem.children);
+let btn = document.getElementById("btn");
+const removeOption = ()=>{
+     for(let i=0;i<allOptions.length;i++){
+          if(selectedElem.value===allOptions[i].value){
+               allOptions[i].remove();
+               return;
+          }
+     }
+}
+
+btn.addEventListener("click",removeOption);
